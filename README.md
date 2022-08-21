b97f9ec37df945c3d4120bb504b3b0504a387ed6
Julian Popescu <hi@julian.dev>
2022-08-21 20:06:25 +0200

    added README
    Signed-off-by: Julian Popescu <hi@julian.dev>

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-# Solidity REPL
+# soli - A Solidity REPL
 
-Run with `cargo run`
+This is a work in progress! See the list of features below [at the bottom of the
+README](#features).
+
+## Installation
+
+Installation requires Rust. You can install the Rust toolchain with
+[rustup](https://rustup.rs/).
+
+```bash
+git clone https://github.com/jpopesculian/soli.git
+cd soli
+cargo install --path .
+```
+
+## Usage
 
 Type some valid Solidity and hope for the best. Type `/help` for some extra commands
+
+![](./assets/demo.gif)
+
+## Features
+
+- [x] Add statements
+- [x] Add custom events, errors, functions, imports
+- [x] Inspect a variable
+- [ ] Fork an existing chain
+- [ ] Allow the use of private keys and custom sender
+- [ ] More advanced introspection
+- [ ] Better error messages and traces
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+//! # A Solidity REPL
+//!
+//! This is a work in progress! See the list of features below [at the bottom of the README](#features).
+//!
+//! ## Installation
+//!
+//! Get rust with [rustup](https://rustup.rs/) and then run `cargo install soli`
+//!
+//!
+//! ## Usage
+//!
+//! Type some valid Solidity and hope for the best. Type `/help` for some extra commands
+//!
+//! ![](../assets/demo.gif)
+//!
+//! ## Features
+//!
+//! - [x] Add statements
+//! - [x] Add custom events, errors, functions, imports
+//! - [x] Inspect a variable
+//! - [ ] Fork an existing chain
+//! - [ ] Allow the use of private keys and custom sender
+//! - [ ] More advanced introspection
+//! - [ ] Better error messages and traces
+
 use std::{collections::HashMap, fmt::Debug, path::PathBuf, sync::atomic};
 
 use ethabi::param_type::ParamType;
@@ -176,6 +201,12 @@ impl<'a> ConstructedSource<'a> {
 
     fn insert_in_constructor(&mut self, content: &str) -> &mut Self {
         self.constructor.push_str(content);
+        self.cached_compiled = None;
+        self
+    }
+
+    fn clear_constructor(&mut self) -> &mut Self {
+        self.constructor = Default::default();
         self.cached_compiled = None;
         self
     }
@@ -749,8 +780,7 @@ fn handle_cmd<'a>(
         }
         "print" => println!("{}", SolidityHelper::highlight(&source.to_string())),
         "clear" => {
-            source.constructor = Default::default();
-            source.cached_compiled = None;
+            source.clear_constructor();
             println!("Stack cleared");
         }
         "inspect" => {
